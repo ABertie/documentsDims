d6129e8479985f2ec60ccecdf8b6625c2422c22d
ABertie <U2439307@rts-365.dk>
2023-05-09 08:26:33 +0200

    start styling

--- README.md
+++ README.md
@@ -5,46 +5,4 @@ Rock Paper Scissors Game
 Vi skal bruge en container med vores spillenavn (og score)
 Så skal vi bruge en anden container med vores sten, saks og papir disse kan være buttons 
 og så skal vi bruge en computer der tilfældigt vælger sten, saks eller papir (og som gemmer og viser vores score i span #score)
-
-
-HTML :
-
-header >
-    h1 > Rock Paper Scissors
-    (p > score 
-        span > (score)
-    )
-/>
-
-main > 
-    button >
-        img/svg/i > sten
-    button > 
-        img/svg/i > saks
-    button >
-        img/svg/i > papir
-/>
-
-JavaScript :
-
-on player chosse by click > 
-
-math.random : 
- if <= 0.33 (33%)
-    rock
- else if <= 0.66 (33%)
-    paper
- else 
-    scissors
-
-
-if computer == player 
-    no one wins
-else if computer == rock && player == scissors
-    computer wins
-else if computer == scissors && player == paper
-    computer wins
-else if computer == paper && player == rock
-    computer wins
-else 
-    player wins
+Hvis der er tid til det også et sted hvor man kan se hvad computeren og spilleren valgte de sidste par gange
--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const PLAYERS__CHOICE = document.querySelectorAll(".playersChoice button")
+const SCORE = document.querySelector("#score")
+const CP__SCORE = document.querySelector("#CPscore")
+const BATTLE = document.querySelector(".battle")
+
+const COMPUTER__UL = document.querySelector(".computersChoice ul")
+const PLAYER__UL = document.querySelector(".playersChoice ul")
+
+const PLAYERS__CHOICE = document.querySelectorAll(".Choice button")
 const ROCK = document.querySelector(".rock")
 const PAPER = document.querySelector(".paper")
 const SCISSORS = document.querySelector(".scissors")
@@ -6,40 +13,60 @@ const SCISSORS = document.querySelector(".scissors")
 let computersChoice
 let playersChoice
 
-PLAYERS__CHOICE.forEach(function (choice){ 
+let score = 0
+SCORE.innerHTML = score
+
+let CPscore = 0
+CP__SCORE.innerHTML = CPscore
+
+PLAYERS__CHOICE.forEach(function (choice) {
     choice.addEventListener("click", function () {
-        letComputerChoose()
-        if (choice == ROCK) playersChoice = "rock"
-        else if (choice == PAPER) playersChoice = "paper"
-        else if (choice == SCISSORS) playersChoice = "scissors"
-        else return
-        whoWins()
+        playersChoice = choice
+        
+        letComputerChose()
+        
+        battle()
     })
 })
 
-function letComputerChoose() {
+function letComputerChose() {
     computersChoice = Math.random()
-    if (computersChoice <= 0.3333) computersChoice = "rock"
-    else if (computersChoice <= 0.6666) computersChoice = "paper"
-    else computersChoice = "scissors"
-    telWhatComputerChosse(computersChoice)
+    if (computersChoice <= 0.3333) computersChoice = ROCK
+    else if (computersChoice <= 0.6666) computersChoice = PAPER
+    else computersChoice = SCISSORS
+}
+
+function createLi(theChoice, ul) {
+    const LI = document.createElement("li")
+    LI.innerHTML = theChoice.innerHTML
+    ul.appendChild(LI)
+}
+
+function battle() {
+    whoWins()
+    BATTLE.style.display = "flex"
+    document.querySelector("#playersChoice").innerHTML = playersChoice.innerHTML
+    document.querySelector("#computersChoice").innerHTML = ""
+    setTimeout(() => {
+        document.querySelector("#computersChoice").innerHTML = computersChoice.innerHTML
+        setTimeout(() => {
+            BATTLE.style.display = "none"
+            SCORE.innerHTML = score
+            CP__SCORE.innerHTML = CPscore
+            createLi(playersChoice, PLAYER__UL)
+            createLi(computersChoice, COMPUTER__UL)
+        }, 2000);
+    }, 1000);
 }
 
 function whoWins() {
     let winner
-    if (computersChoice == playersChoice ) winner = "no one"
-    else if (computersChoice == "rock" && playersChoice == "scissors") winner = "computer"
-    else if (computersChoice == "paper" && playersChoice == "rock") winner = "computer"
-    else if (computersChoice == "scissors" && playersChoice == "paper") winner = "computer"
-    else winner = "player" 
-    tellWhoWon(winner)
+    if (computersChoice === playersChoice) winner = "no one"
+    else if (computersChoice === ROCK && playersChoice === SCISSORS
+        || computersChoice === PAPER && playersChoice === ROCK
+        || computersChoice === SCISSORS && playersChoice === PAPER) winner = "computer"
+    else winner = "player"
+
+    if (winner === "computer") CPscore = CPscore + 1
+    if (winner === "player") score = score + 1
 }
-
-function tellWhoWon(winner) {
-    if (winner == "computer") alert("you lost! try again")
-    else if (winner == "player") alert("you won")
-    else if (winner == "no one") alert("no winner! try again")
-}
-
-
-
