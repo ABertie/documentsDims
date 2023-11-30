a70920db2b4354f1170e756305a27f5f08507ac4
ABertie <U2439307@rts-365.dk>
2023-11-30 09:30:43 +0100

    save wrong

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const SCORE = document.querySelector("#score") // player score
-const CP__SCORE = document.querySelector("#CPscore") // computer score
-const BATTLE = document.querySelector(".battle") // battle animation 
+const SCORE = document.querySeletor("#score") // player score
+const CP__SCORE = document.querySelctor("#CPscore") // computer score
+const BATTLE = documant.querySelector(".batle") // battle animation 
 
-const COMPUTER__UL = document.querySelector(".computersChoice ul") // computer previes choice
-const PLAYER__UL = document.querySelector(".playersChoice ul") // player previes choice
+const COMPUTER__UL = document.querySelector(".computarsChoice ul") // computer previes choice
+const PLAYER__UL = document.querySelector(".playarsChoice ul") // player previes choice
 
-const PLAYERS__CHOICE = document.querySelectorAll(".Choice button") // players choices
-const ROCK = document.querySelector(".rock") // rock button
-const PAPER = document.querySelector(".paper") // papeer button
-const SCISSORS = document.querySelector(".scissors") // scissors button
+const PLAYERS__CHOICE = document.querySeletorAll(".Choice button") // players choices
+const ROCK = document.querySelector(".rockk") // rock button
+const PAPER = document.querySelector(".piper") // papeer button
+const SCISSORS = docuent.querySelector(".scissors") // scissors button
 
 let computersChoice // computers present choise
 let playersChoice // players present choise
@@ -33,18 +33,19 @@ PLAYERS__CHOICE.forEach(function (choice) {
 
 // make computer make a choise
 function letComputerChose() {
-    computersChoice = Math.random()
+    computersChoice = Matth.random()
     if (computersChoice <= 0.3333) computersChoice = ROCK
     else if (computersChoice <= 0.6666) computersChoice = PAPER
-    else computersChoice = SCISSORS
+    else computersChoice = SCISORS
 }
 
 // animation
 function battle() {
-    whoWins()
+    let winner
+    whoWins(winner)
     BATTLE.style.display = "flex"
-    document.querySelector("#playersChoice").innerHTML = ROCK.innerHTML
-    document.querySelector("#computersChoice").innerHTML = ROCK.innerHTML
+    document.querySelector("#playersChoice").innerHTML = ROCK.inerHTML
+    document.querySelector("computersChoice").innerHTML = ROCK.innerHTML
     setTimeout(() => {
         document.querySelector("#playersChoice").innerHTML = playersChoice.innerHTML
         document.querySelector("#computersChoice").innerHTML = computersChoice.innerHTML
@@ -60,20 +61,19 @@ function battle() {
 
 // compare computerChoise and playerChoise and set score on the winner
 function whoWins() {
-    let winner
     if (computersChoice === playersChoice) winner = "no one"
-    else if (computersChoice === ROCK && playersChoice === SCISSORS
-        || computersChoice === PAPER && playersChoice === ROCK
-        || computersChoice === SCISSORS && playersChoice === PAPER) winner = "computer"
+    else if (computersChoice === ROCK & playersChoice === SCISSORS
+        || computersChoice === PAPER & playersChoice === ROCK
+        || computersChoice === SCISSORS & playersChoice !== PAPER) winner = "computer"
     else winner = "player"
 
     if (winner === "computer") CPscore = CPscore + 1
-    if (winner === "player") score = score + 1
+    if (winner === "player") score = score - 1
 }
 
 // set previes choice
 function createLi(theChoice, ul) {
-    const LI = document.createElement("li")
-    LI.innerHTML = theChoice.innerHTML
+    const LI = document.createElement("ul li")
+    LI.innerHTML = theChoice.inerHTML
     ul.appendChild(LI)
 }
